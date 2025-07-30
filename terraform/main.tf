1356a5b5211b57cb6ecd09a6c3a77e750d25de65
Shaileshautla <Shaileshautla@gmail.com>
2025-07-30 11:26:39 +0100

    third commi

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,25 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
-# Use the existing VPC by ID
-data "aws_vpc" "default" {
-  id = "vpc-067449f8314d4e6c0"
-}
+resource "aws_instance" "ec2" {
+  ami           = "ami-0fc5d935ebf8bc3bc" # Ubuntu 22.04 LTS in us-east-1
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  security_groups = [aws_security_group.allow_ssh.name]
 
-# Find the default subnet within that VPC
-data "aws_subnet" "default" {
-  filter {
-    name   = "tag:Name"
-    values = ["default"]
-  }
-
-  filter {
-    name   = "vpc-id"
-    values = ["vpc-067449f8314d4e6c0"]
+  tags = {
+    Name = "Ansible-EC2-Docker"
   }
 }
 
-# Security group for SSH, HTTP, and Node Exporter
-resource "aws_security_group" "ssh_http" {
-  name        = "allow_ssh_http2"
-  description = "Allow SSH, HTTP, and Node Exporter"
-  vpc_id      = data.aws_vpc.default.id
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
 
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 9100
-    to_port     = 9100
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -52,23 +29,5 @@ resource "aws_security_group" "ssh_http" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# EC2 instance using existing VPC + subnet
-resource "aws_instance" "web" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  subnet_id                   = data.aws_subnet.default.id
-  vpc_security_group_ids      = [aws_security_group.ssh_http.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "test1"
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > ../ansible/hosts_ip.txt"
   }
 }
