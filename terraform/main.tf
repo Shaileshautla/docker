ec758b979f4a1d6c2e6718a6992009b90bb862ef
Shaileshautla <Shaileshautla@gmail.com>
2025-07-30 11:33:13 +0100

    third com

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,44 +2,42 @@ provider "aws" {
   region = var.region
 }
 
-# Use the existing VPC by ID
+# Use an existing VPC by ID
 data "aws_vpc" "default" {
   id = "vpc-067449f8314d4e6c0"
 }
 
-# Find the default subnet within that VPC
+# Find a subnet in that VPC (e.g., default named subnet)
 data "aws_subnet" "default" {
   filter {
-    name   = "tag:Name"
-    values = ["default"]
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 
   filter {
-    name   = "vpc-id"
-    values = ["vpc-067449f8314d4e6c0"]
+    name   = "default-for-az"
+    values = ["true"]
   }
 }
 
-resource "aws_instance" "ec2" {
-  ami           = "ami-0fc5d935ebf8bc3bc" # Ubuntu 22.04 LTS in us-east-1
-  instance_type = "t2.micro"
-  key_name      = var.key_name
-  subnet_id                   = data.aws_subnet.default.id
-  vpc_security_group_ids      = [aws_security_group.ssh_http.id]
-  security_groups = [aws_security_group.allow_ssh.name]
-
-  tags = {
-    Name = "Ansible-EC2-Docker"
-  }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+# Security group allowing SSH (port 22) and optionally HTTP (port 80)
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -50,4 +48,22 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# Launch EC2 instance
+resource "aws_instance" "ec2" {
+  ami                         = "ami-0fc5d935ebf8bc3bc" # Ubuntu 22.04 LTS in us-east-1
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = data.aws_subnet.default.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "Ansible-EC2-Docker"
+  }
+}
+
+# Output EC2 Public IP
+output "ec2_public_ip" {
+  value = aws_instance.ec2.public_ip
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,14 +2,8 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "ami" {
-  default = "ami-0c02fb55956c7d316"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
 variable "key_name" {
-  default = "test" # actual key name created in AWS, no .pem extension
+  description = "The name of your AWS EC2 Key Pair"
+  type        = string
+  default = test
 }
