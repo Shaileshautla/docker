f9d4f6c701e8047ad532dab1d7fc92535a0a9995
Shaileshautla <97507999+Shaileshautla@users.noreply.github.com>
2025-07-30 12:00:55 +0100

    Update run-playbook.sh

--- ansible-ec2-docker/run-playbook.sh
+++ ansible-ec2-docker/run-playbook.sh
@@ -3,6 +3,7 @@
 # Go to terraform directory and get EC2 IP
 cd ../terraform || exit
 EC2_IP=$(terraform -chdir=terraform output -raw ec2_public_ip)
+echo "Target EC2 IP: $EC2_IP"
 
 # Go to ansible directory
 cd ../ansible-ec2-docker || exit
@@ -13,3 +14,4 @@ echo "$EC2_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/test.pem" 
 
 # Run the Ansible playbook
 ansible-playbook -i inventory.ini install_docker.yml
+
