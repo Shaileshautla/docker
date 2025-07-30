d76b9da5767256536265e3c1108b5af87cb71f57
Shaileshautla <Shaileshautla@gmail.com>
2025-07-30 11:51:07 +0100

    thi

--- ansible-ec2-docker/run-playbook.sh
+++ ansible-ec2-docker/run-playbook.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-cd ../terraform
+# Go to terraform directory and get EC2 IP
+cd ../terraform || exit
 EC2_IP=$(terraform output -raw ec2_public_ip)
-cd ../ansible-ec2-docker
 
-echo "[ec2]" > inventory.ini
+# Go to ansible directory
+cd ../ansible-ec2-docker || exit
+
+# Create Ansible inventory file
+echo "[web]" > inventory.ini
 echo "$EC2_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/test.pem" >> inventory.ini
 
+# Run the Ansible playbook
 ansible-playbook -i inventory.ini install_docker.yml
